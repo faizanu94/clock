080b799b33f85a61cfa0ff9e7d7f957c032289c5
unknown <faizan.uddin94@gmail.com>
2023-02-01 19:25:26 +0400

    initial commit

---
+++ src/clock/index.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import './style.css';
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const timers = {
+    'hours': (date.getHours() % 12) * 30,
+    'minutes': date.getMinutes() * 6,
+    'seconds': date.getSeconds() * 6
+  };
+
+  return (
+    <div className="clock">
+      <div className="dial"></div>
+      {Object.keys(timers).map((timer, index) => <div key={index} className={timer} style={{ transform: `rotate(${timers[timer]}deg)` }} />)}
+    </div>
+  );
+}
+
+export default Clock;
