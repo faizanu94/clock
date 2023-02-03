21eeff615e86addb97c8026ad8156e9fff44b48f
Muhammad Faizan Uddin <faizan.uddin94@gmail.com>
2023-02-03 12:04:43 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="logo">
+        <a href="https://github.com/faizanu94/clock" target="_blank" rel="noreferrer">
+          <img className="logo-img" alt="github-logo" src="./logo/github-mark.svg" width={30} height={30}/>
+        </a>
+      </div>
       <Clock />
     </div>
   );
